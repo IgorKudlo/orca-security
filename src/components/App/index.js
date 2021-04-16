34eea70cb46b131b01ff633ef361ca9ed9a8d51b
IgorKudlo <igoreklida@gmail.com>
2021-04-16 23:02:55 +0300

    add variables, slice image

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from './styles.module.scss';
 
 function App() {
   return (
-    <div>
-      Orca security
+    <div className={styles["app"]}>
+      <div className={styles["app__container"]}>
+        Orca security
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
