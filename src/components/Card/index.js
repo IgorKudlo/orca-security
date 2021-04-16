a727afe9550fb4f8d795681d875da31d7bc265e0
IgorKudlo <igoreklida@gmail.com>
2021-04-17 01:30:44 +0300

    add card menu

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '../Button';
 import sprite from '../../img/sprite.svg';
 import styles from './styles.module.scss';
 
 const Card = ({ title, name, type, tests, addClass }) => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const openMenuHandler = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className={`${styles['card']} ${addClass || ''}`}>
       <div className={styles['card__header']}>
         <span className={styles['card__title']}>{title}</span>
         <div className={styles['card__menu']}>
-          <button className={styles['card__menu-btn']}>
+          <button className={styles['card__menu-btn']} onClick={openMenuHandler}>
             <svg className={styles['card__menu-btn-icon']} width={3} height={12}>
               <use xlinkHref={`${sprite}#dots`}/>
             </svg>
           </button>
+          {isMenuOpen && (
+            <div className={styles['card__menu-box']}>
+              <button className={styles['card__menu-close']} onClick={openMenuHandler}>
+                <svg className={styles['card__menu-close-icon']} width={11} height={11}>
+                  <use xlinkHref={`${sprite}#close`}/>
+                </svg>
+              </button>
+              <ul className={styles['card__menu-list']}>
+                <li className={`${styles['card__menu-list-item']} ${styles['card__menu-item']}`}>
+                  <button className={styles['card__menu-item-btn']}>
+                    <svg className={styles['card__menu-item-icon']} width={10} height={12}>
+                      <use xlinkHref={`${sprite}#copy`}/>
+                    </svg>
+                    Duplicate
+                  </button>
+                </li>
+                <li className={`${styles['card__menu-list-item']} ${styles['card__menu-item']}`}>
+                  <button className={styles['card__menu-item-btn']}>
+                    <svg className={styles['card__menu-item-icon']} width={12} height={12}>
+                      <use xlinkHref={`${sprite}#edit`}/>
+                    </svg>
+                    Edit
+                  </button>
+                </li>
+                <li className={`${styles['card__menu-list-item']} ${styles['card__menu-item']}`}>
+                  <button className={styles['card__menu-item-btn']}>
+                    <svg className={styles['card__menu-item-icon']} width={10} height={12}>
+                      <use xlinkHref={`${sprite}#delete`}/>
+                    </svg>
+                    Delete
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
       <div className={styles['card__body']}>
